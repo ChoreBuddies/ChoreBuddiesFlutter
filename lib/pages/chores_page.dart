138f703c9f1403552036d89f29a60ac75decbfa1
Avesay <juliak@jkkomputer.com.pl>
2026-01-09 13:32:28 +0100

    Add page title

--- lib/pages/chores_page.dart
+++ lib/pages/chores_page.dart
@@ -20,37 +20,40 @@ class _ChoresPageState extends State<ChoresPage> {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Center(
-        child: FutureBuilder<List<ChoreOverview>>(
-          future: choreService.getChores(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const CircularProgressIndicator();
-            } else if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-              return const Text('No chores found');
-            }
+        child: Scaffold(
+        appBar: AppBar(title: const Text("My Chores")),
+        body:        FutureBuilder<List<ChoreOverview>>(
+            future: choreService.getChores(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              } else if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                return const Text('No chores found, add a new one to see it here!');
+              }
 
-            final chores = snapshot.data!;
+              final chores = snapshot.data!;
 
-            return ListView.separated(
-              itemCount: chores.length,
-              separatorBuilder: (context, index) => const Divider(
-                height: 1,
-              ),
-              itemBuilder: (context, index) {
-                return ChoreView(
-                  choreOverview: chores[index],
-                  onChanged: (value) => setState(() {
-                    if (value != null && value) {
-                      choreService.markChoreAsDone(chores[index]);
-                    }
-                  }),
-                );
-              },
-            );
-          },
-        ),
+              return ListView.separated(
+                itemCount: chores.length,
+                separatorBuilder: (context, index) => const Divider(
+                  height: 1,
+                ),
+                itemBuilder: (context, index) {
+                  return ChoreView(
+                    choreOverview: chores[index],
+                    onChanged: (value) => setState(() {
+                      if (value != null && value) {
+                        choreService.markChoreAsDone(chores[index]);
+                      }
+                    }),
+                  );
+                },
+              );
+            },
+          ),
+        )
       ),
     );
   }
