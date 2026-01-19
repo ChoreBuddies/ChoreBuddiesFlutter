dba7628e75b033f33b8aa92e2cca95043442f37a
Avesay <juliak@jkkomputer.com.pl>
2026-01-19 20:52:13 +0100

    build runner build

--- lib/features/households/models/household.g.dart
+++ lib/features/households/models/household.g.dart
@@ -7,9 +7,9 @@ part of 'household.dart';
 // **************************************************************************
 
 Household _$HouseholdFromJson(Map<String, dynamic> json) => Household(
-  (json['id'] as num).toInt(),
-  json['name'] as String,
-  json['description'] as String,
+  (json['id'] as num?)?.toInt(),
+  json['name'] as String?,
+  json['description'] as String?,
 );
 
 Map<String, dynamic> _$HouseholdToJson(Household instance) => <String, dynamic>{
