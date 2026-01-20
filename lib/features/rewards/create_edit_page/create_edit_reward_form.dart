ea4d0b9fe0f68eb900669274dbcddd4bfefc9694
Avesay <juliak@jkkomputer.com.pl>
2026-01-20 02:09:16 +0100

    create/edit page

---
+++ lib/features/rewards/create_edit_page/create_edit_reward_form.dart
@@ -0,0 +1,183 @@
+import 'package:chorebuddies_flutter/UI/styles/button_styles.dart';
+import 'package:chorebuddies_flutter/UI/widgets/g_form_field.dart';
+import 'package:chorebuddies_flutter/features/rewards/create_edit_page/create_edit_reward_page.dart';
+import 'package:chorebuddies_flutter/features/rewards/models/Reward.dart';
+import 'package:chorebuddies_flutter/utils/validators.dart';
+import 'package:flutter/material.dart';
+
+class CreateEditRewardForm extends StatefulWidget {
+  final Reward model;
+  final PageMode pageMode;
+  final ValueChanged<Reward>? onValidSubmit;
+  final ValueChanged<PageMode>? onPageModeChanged;
+  final bool showCancel;
+
+  const CreateEditRewardForm({
+    super.key,
+    required this.model,
+    required this.pageMode,
+    this.onValidSubmit,
+    this.onPageModeChanged,
+    this.showCancel = true,
+  });
+
+  @override
+  State<CreateEditRewardForm> createState() => _CreateEditRewardFormState();
+}
+
+class _CreateEditRewardFormState extends State<CreateEditRewardForm> {
+  bool _isLoading = false;
+
+  late final TextEditingController nameController;
+  late final TextEditingController descriptionController;
+  late final TextEditingController costController;
+  late final TextEditingController quantityController;
+
+  late PageMode mode;
+
+  final _formKey = GlobalKey<FormState>();
+  void _onSave() {
+    _isLoading = true;
+    if (_formKey.currentState?.validate() ?? false) {
+      final model = widget.model;
+      model.name = nameController.text;
+      model.description = descriptionController.text;
+      model.cost = int.tryParse(costController.text) ?? 0;
+      model.quantityAvailable = int.tryParse(quantityController.text) ?? 0;
+
+      widget.onValidSubmit?.call(model);
+    }
+    _isLoading = false;
+    if (mode == PageMode.create) return;
+    setState(() => mode = PageMode.view);
+    widget.onPageModeChanged?.call(mode);
+  }
+
+  Future<void> _switchToEdit() async {
+    setState(() => mode = PageMode.edit);
+    widget.onPageModeChanged?.call(mode);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    mode = widget.pageMode;
+    final model = widget.model;
+    nameController = TextEditingController(text: model.name);
+    descriptionController = TextEditingController(text: model.description);
+    costController = TextEditingController(text: model.cost.toString());
+    quantityController = TextEditingController(
+      text: model.quantityAvailable.toString(),
+    );
+  }
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    descriptionController.dispose();
+    super.dispose();
+  }
+
+  bool get _readOnly => mode == PageMode.view;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: ListView(
+        padding: const EdgeInsets.all(16),
+        children: [
+          GFormField(
+            labelText: 'Name',
+            controller: nameController,
+            readonly: mode == PageMode.view,
+            validator: (v) => v == null || v.trim().isEmpty ? 'Required' : null,
+          ),
+
+          GFormField(
+            labelText: 'Description',
+            controller: descriptionController,
+            readonly: mode == PageMode.view,
+            maxLines: null,
+          ),
+
+          GFormField(
+            labelText: 'Cost',
+            controller: costController,
+            keyboardType: TextInputType.number,
+            readonly: mode == PageMode.view,
+            validator: (value) {
+              if (_readOnly) return null;
+              if (value == null || value.isEmpty) return 'Required';
+              return Validators.validate(
+                value,
+                ValidationType.nonNegativeInteger,
+              );
+            },
+          ),
+
+          GFormField(
+            labelText: 'Quantity Available',
+            controller: quantityController,
+            keyboardType: TextInputType.number,
+            readonly: mode == PageMode.view,
+            validator: (value) {
+              if (_readOnly) return null;
+              if (value == null || value.isEmpty) return 'Required';
+              return Validators.validate(
+                value,
+                ValidationType.nonNegativeInteger,
+              );
+            },
+          ),
+
+          const SizedBox(height: 24),
+
+          if (mode == PageMode.view)
+            Row(
+              children: [
+                const SizedBox(height: 10),
+                Expanded(
+                  child: OutlinedButton.icon(
+                    onPressed: () => setState(() => _switchToEdit()),
+                    icon: const Icon(Icons.edit),
+                    label: const Text('Edit'),
+                  ),
+                ),
+                const SizedBox(width: 12),
+              ],
+            ),
+          if (mode != PageMode.view)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (mode == PageMode.edit)
+                  ElevatedButton(
+                    onPressed: () => setState(() {
+                      mode = PageMode.view;
+                    }),
+                    style: ElevatedButtonStyles.cancelStyle,
+                    child: const Text('Cancel'),
+                  ),
+                const SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () => _onSave(),
+                  child: _isLoading
+                      ? const SizedBox(
+                          width: 20,
+                          height: 10,
+                          child: CircularProgressIndicator(
+                            strokeWidth: 2,
+                            color: Colors.white,
+                          ),
+                        )
+                      : const Text('Save'),
+                ),
+              ],
+            ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/features/rewards/create_edit_page/create_edit_reward_page.dart
@@ -0,0 +1,84 @@
+import 'package:chorebuddies_flutter/features/rewards/create_edit_page/create_edit_reward_form.dart';
+import 'package:chorebuddies_flutter/features/rewards/models/Reward.dart';
+import 'package:chorebuddies_flutter/features/rewards/reward_service.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+enum PageMode { create, view, edit }
+
+class CreateEditRewardPage extends StatefulWidget {
+  final int? rewardId;
+
+  const CreateEditRewardPage({super.key, this.rewardId});
+
+  @override
+  State<CreateEditRewardPage> createState() =>
+      _CreateEditRewardPageState();
+}
+
+class _CreateEditRewardPageState extends State<CreateEditRewardPage> {
+  late Reward model;
+  late PageMode pageMode;
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  Future<void> _loadData() async {
+    var rewardService = context.read<RewardService>();
+    if (widget.rewardId != null) {
+      pageMode = PageMode.view;
+      model = await rewardService.getReward(widget.rewardId!) ?? Reward(null, '', '', 0, 0, 0);
+    } else {
+      pageMode = PageMode.create;
+      model = Reward(null, '', '', 0, 0, 0);
+    }
+
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  Future<void> _handleSave(Reward updatedModel) async {
+    var rewardService = context.read<RewardService>();
+    var result;
+    if (pageMode == PageMode.edit) {
+      result = await rewardService.updateReward(updatedModel);
+    } else {
+      result = await rewardService.createReward(updatedModel);
+    }
+    if(!mounted) return;
+    if (result != null) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text('Saved successfully!')));
+        setState(() {
+          pageMode = PageMode.view;
+        });
+    } else {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text('Error occured')));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (isLoading) {
+      return const Scaffold(body: Center(child: CircularProgressIndicator()));
+    }
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Reward Details')),
+      body: CreateEditRewardForm(
+        model: model,
+        pageMode: pageMode,
+        onValidSubmit: _handleSave,
+        onPageModeChanged: (newMode) => setState(() => pageMode = newMode),
+      ),
+    );
+  }
+}
