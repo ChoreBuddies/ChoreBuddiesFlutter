c4ad4fb938b669e186736afea7e858f512fdf682
Kobiuy <132697518+Kobiuy@users.noreply.github.com>
2026-01-25 11:46:34 +0100

    fix formatter

--- lib/utils/formatters.dart
+++ lib/utils/formatters.dart
@@ -2,7 +2,7 @@ import 'package:chorebuddies_flutter/features/notifications/models/notificaiton_
 
 String formatDate(DateTime? date) {
   if (date == null) return '';
-  return '${date.year}-${date.month.toString().padLeft(2, '0')}-${date.day.toString().padLeft(2, '0')}';
+  return '${date.day.toString().padLeft(2, '0')}.${date.month.toString().padLeft(2, '0')}.${date.year}';
 }
 
 String formatEventName(NotificationEvent event) {
